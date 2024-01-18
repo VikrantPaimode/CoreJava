5712ec710154560025f0e580f2a16a65b88bb5c0
o <o@LENOVO-pc-20>
2024-01-18 13:27:44 +0530

    First Commit

---
+++ IntroToJava/src/com/edubridge/compileinput/CompileInputDemo.java
@@ -0,0 +1,27 @@
+package com.edubridge.compileinput;
+//program to demonstrate on CompileTime Input for the data types
+public class CompileInputDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+byte num1=127;
+short num2=3433;
+int num3=345671116;
+long num4=9834567890L;
+char c='X';
+String str="EduBridge";
+float num5=54.6f;
+double num=54.5d;
+System.out.println(num1);
+System.out.println(num2);
+System.out.println(num3);
+System.out.println(num4);
+System.out.println(num5);
+System.out.println(num);
+System.out.println(str);
+System.out.println(c);
+
+
+ 	}
+
+}
